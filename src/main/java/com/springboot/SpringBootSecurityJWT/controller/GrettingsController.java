d88ca8463a7fc7e09b11865ba4253f3cfa503813
Monu-Chaudhary <chaudharymonu45@gmail.com>
2023-05-17 14:46:31 -0400

    created a sample springboot project

---
+++ src/main/java/com/springboot/SpringBootSecurityJWT/controller/GrettingsController.java
@@ -0,0 +1,22 @@
+package com.springboot.SpringBootSecurityJWT.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/greetings")
+public class GrettingsController {
+
+    @GetMapping
+    public ResponseEntity<String> hello() {
+        return ResponseEntity.ok("Hello");
+    }
+
+    @GetMapping("/bye")
+    public ResponseEntity<String> bye() {
+        return ResponseEntity.ok("Bye");
+    }
+
+}
